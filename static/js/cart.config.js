7fd1662630fc8642a3ae5554ff240f2548851dc6
WayoCarrasco <eduardo.carrasco.nz@gmail.com>
2018-03-06 12:22:20 -0300

    bodega ondev

--- static/js/cart.config.js
+++ static/js/cart.config.js
@@ -43,11 +43,11 @@ $(document).ready(function()
 
     var base_url = $.environmentVar(
         'https://apibodegas.loadingplay.com/',
-        'https://apibodegas.ondev.today/',
+        'https://apibodegas.loadingplay.com/',
         'https://apibodegas.loadingplay.com/');
     var checkout_url = $.environmentVar(
         'https://pay.loadingplay.com/',
-        'https://lpcheckout.ondev.today/',
+        'https://pay.loadingplay.com/',
         'https://pay.loadingplay.com');
     var app_public = $.environmentVar(82,82,82);
     var site_name = $.environmentVar('gustocasero', 'gustocasero', 'gustocasero');

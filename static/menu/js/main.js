a38f74ee3ffdcc347ad50c0572f2d762387b0a47
WayoCarrasco <eduardo.carrasco.nz@gmail.com>
2018-06-12 11:20:45 -0400

    fix header mobile

--- static/menu/js/main.js
+++ static/menu/js/main.js
@@ -1,18 +1,19 @@
 // import vendor/modernizr.js
 // import vendor/jquery.js
 
-$(document).ready(function(){
+$(document).ready(function()
+{
+    $(".nav-main li:has(ul)").addClass("has-sub-nav").prepend("<div class=\"sub-toggle\"></div>");
 
-	$(".nav-main li:has(ul)").addClass("has-sub-nav").prepend("<div class=\"sub-toggle\"></div>");
+    $(".has-sub-nav a").click(function()
+    {
+        $(this).parent().addClass("active");
+        $(".nav-container").addClass("show-sub");
+    });
 
-	$(".has-sub-nav a").click(function(){
-		$(this).parent().addClass("active");
-		$(".nav-container").addClass("show-sub");
-	});
-
-	$(".has-sub-nav .back").click(function(){
-		$(".nav-container").removeClass("show-sub");
-		$(".has-sub-nav").removeClass("active");
-	});
-
+    $(".has-sub-nav .back").click(function()
+    {
+        $(".nav-container").removeClass("show-sub");
+        $(".has-sub-nav").removeClass("active");
+    });
 });
